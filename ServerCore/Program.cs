3db934618c08d50c7c9e79f85f0f2f5cf8d8ea61
King0wl <70518135+yhj0076@users.noreply.github.com>
2024-04-09 19:09:22 +0900

    캐시 이론

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -5,35 +5,28 @@ namespace ServerCore
 {
     class Program
     {
-        volatile static bool _stop = false;
-
-        static void ThreadMain()
-        {
-            Console.WriteLine("쓰레드 시작!");
-
-            while(_stop == false)
-            {
-                // 누군가가 stop 신호를 해주기를 기다린다
-            }
-
-            Console.WriteLine("쓰레드 종료!");
-        }
-
         static void Main(string[] args)
         {
-            Task t = new Task(ThreadMain);
-            t.Start();
+            int[,] arr = new int[10000, 10000];
 
-            Thread.Sleep(1000);
+            {
+                long now = DateTime.Now.Ticks;
+                for (int y = 0; y < 10000; y++)
+                    for(int x = 0; x < 10000;  x++)
+                        arr[y, x] = 1;
+                long end = DateTime.Now.Ticks;
+                Console.WriteLine($"(y,x) 순서 걸린 시간 {end - now}");
+            }
 
-            _stop = true;
-
-            Console.WriteLine("Stop 호출");
-            Console.WriteLine("종료 대기중");
-
-            t.Wait();
-
-            Console.WriteLine("종료 성공");
+            {
+                long now = DateTime.Now.Ticks;
+                for (int y = 0;y <10000; y++)
+                    for (int x = 0;x <10000;  x++)
+                        arr[x, y] = 1;
+                long end = DateTime.Now.Ticks;
+                Console.WriteLine($"(y,x) 순서 걸린 시간 {end - now}");
+            }
+            // special locality
         }
     }
 }
