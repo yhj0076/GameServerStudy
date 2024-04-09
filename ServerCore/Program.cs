d693f1fafff762b9859b009fff23a80ad8dab284
King0wl <70518135+yhj0076@users.noreply.github.com>
2024-04-09 17:02:27 +0900

    first commit

---
+++ ServerCore/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace ServerCore
+{
+    class Program
+    {
+        static void MainThread(object state)
+        {
+            for(int i = 0; i < 5; i++)
+            Console.WriteLine("Hello Thread!");
+        }
+
+        static void Main(string[] args)
+        {
+            ThreadPool.SetMinThreads(1, 1);
+            ThreadPool.SetMaxThreads(5, 5);
+
+            for (int i = 0; i < 5; i++)
+                ThreadPool.QueueUserWorkItem((obj) => { while (true) { } });
+
+            ThreadPool.QueueUserWorkItem(MainThread);
+
+            //Thread t = new Thread(MainThread);
+            //t.Name = "test Thread";
+            //t.IsBackground = true; 
+            //t.Start();
+            //Console.WriteLine("Wiating for Thread");
+
+            //t.Join();
+
+            //Console.WriteLine("Hello World!");
+            while (true) { }
+        }
+    }
+}
