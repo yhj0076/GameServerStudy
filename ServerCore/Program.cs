3455777fff6ecec8e9cab6ab71358bcb2465907d
King0wl <70518135+yhj0076@users.noreply.github.com>
2024-04-09 17:54:15 +0900

    컴파일러 최적화
    release 모드에서는 코드 최적화가 일어난다.
    그래서 없던 버그가 생길 수도 있다.

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -5,32 +5,35 @@ namespace ServerCore
 {
     class Program
     {
-        static void MainThread(object state)
+        volatile static bool _stop = false;
+
+        static void ThreadMain()
         {
-            for(int i = 0; i < 5; i++)
-            Console.WriteLine("Hello Thread!");
+            Console.WriteLine("쓰레드 시작!");
+
+            while(_stop == false)
+            {
+                // 누군가가 stop 신호를 해주기를 기다린다
+            }
+
+            Console.WriteLine("쓰레드 종료!");
         }
 
         static void Main(string[] args)
         {
-            ThreadPool.SetMinThreads(1, 1);
-            ThreadPool.SetMaxThreads(5, 5);
+            Task t = new Task(ThreadMain);
+            t.Start();
 
-            for (int i = 0; i < 5; i++)
-                ThreadPool.QueueUserWorkItem((obj) => { while (true) { } });
+            Thread.Sleep(1000);
 
-            ThreadPool.QueueUserWorkItem(MainThread);
+            _stop = true;
 
-            //Thread t = new Thread(MainThread);
-            //t.Name = "test Thread";
-            //t.IsBackground = true; 
-            //t.Start();
-            //Console.WriteLine("Wiating for Thread");
+            Console.WriteLine("Stop 호출");
+            Console.WriteLine("종료 대기중");
 
-            //t.Join();
+            t.Wait();
 
-            //Console.WriteLine("Hello World!");
-            while (true) { }
+            Console.WriteLine("종료 성공");
         }
     }
 }
