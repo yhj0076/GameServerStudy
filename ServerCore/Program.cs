7317999598ce980980ac4b73858e6b0b3c0c40bb
King0wl <70518135+yhj0076@users.noreply.github.com>
2024-05-06 22:16:04 +0900

    Interlocked 추가

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -6,16 +6,17 @@ namespace ServerCore
 {
     class Program
     {
-        static volatile int number = 0;
+        static int number = 0;
 
         static void Thread_1()
         {
             // atomic = 원자성
 
-            // Interlock : 원자성을 보장해 줄 수 있지만 성능에서 손해를 보게 된다
+            // Interlock : 원자성을 보장해 줄 수 있는 함수. 성능에서 손해를 보게 된다. 순서가 보장된다.
             for(int i = 0; i < 1000000; i++)
             {
-                Interlocked.Increment(ref number);
+                // All or Nothing
+                Interlocked.Increment(ref number);  // ref -> number의 주소값을 참조한다.
             } 
         }
 
