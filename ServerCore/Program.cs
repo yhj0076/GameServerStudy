11f1ab67ae6d7370a3dc66f50116d10b2acf95bf
King0wl <70518135+yhj0076@users.noreply.github.com>
2024-08-03 17:56:37 +0900

    ReaderWriterLock
    실습 전

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -4,63 +4,24 @@ using System.Threading;
 
 namespace ServerCore
 {
-    /*class Lock
-    {
+    /* 락을 표현하는 방법
+     * 
+     * 1. 근성 : 계속 문을 열기를 시도하는 것
+     * 2. 양보 : 조금 있다가 다시 오겠다.
+     * 3. 갑질 : 나 대신 너가 이거 문 열리는 지 보고 있어라.
+     * 
+     * 스핀락 : 근성과 양보를 서로서로 사용한다.
+     * 뮤텍스 : 커널을 이용해서 스핀락보다는 느리다.
+     * 
+     * RWLock(ReaderWriterLock)
+     */
 
-        // 커널을 이용하면 속도가 겁나게 느리다.
-        // bool == 커널
-        // AutoResetEvent _available = new AutoResetEvent(true);   // 톨게이트 개념. true면 열려있고 false면 닫혀있다.
-
-        ManualResetEvent _available = new ManualResetEvent(true);   // 방문 개념.
-
-        public void Acquire()
-        {
-            _available.WaitOne();   // 입장 시도
-            _available.Reset(); // 문을 닫는다.
-        }
-
-        public void Release() 
-        {
-            _available.Set();   // 문을 열어준다.
-        }
-    }*/
 
     class Program
     {
-        static int _num = 0;
-        //static Lock _lock = new Lock();
-        static Mutex _lock = new Mutex();
-
-        static void Thread_1()
-        {
-            for(int i = 0; i < 1000000000; i++) 
-            {
-                _lock.WaitOne();
-                _num++;
-                _lock.ReleaseMutex();
-            }
-        }
-
-        static void Thread_2()
-        {
-            for (int i = 0; i < 1000000000; i++)
-            {
-                _lock.WaitOne();
-                _num--;
-                _lock.ReleaseMutex();
-            }
-        }
-
         static void Main(string[] args)
         {
-            Task t1 = new Task(Thread_1);
-            Task t2 = new Task(Thread_2);
-            t1.Start();
-            t2.Start();
 
-            Task.WaitAll(t1, t2);
-
-            Console.WriteLine(_num);
         }
     }
 }
