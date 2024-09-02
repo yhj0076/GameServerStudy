8f347d523689cec1f0ca65b69d9ade9487dce706
King0wl <70518135+yhj0076@users.noreply.github.com>
2024-09-02 12:03:37 +0900

    TLS

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -4,45 +4,28 @@ using System.Threading;
 
 namespace ServerCore
 {
-    /* 락을 표현하는 방법
-     * 
-     * 1. 근성 : 계속 문을 열기를 시도하는 것
-     * 2. 양보 : 조금 있다가 다시 오겠다.
-     * 3. 갑질 : 나 대신 너가 이거 문 열리는 지 보고 있어라.
-     * 
-     * 스핀락 : 근성과 양보를 서로서로 사용한다.
-     * 뮤텍스 : 커널을 이용해서 스핀락보다는 느리다.
-     * 
-     * RWLock(ReaderWriterLock)
-     */
-
-
     class Program
     {
-        static volatile int count = 0;
-        static Lock _lock = new Lock();
+        static ThreadLocal<string> ThreadName = new ThreadLocal<string>(() => { return $"My Name Is {Thread.CurrentThread.ManagedThreadId}"; });
+        static void WhoAmI()
+        {
+            bool repeat = ThreadName.IsValueCreated;
+            if (repeat) 
+            {
+                Console.WriteLine(ThreadName.Value + "{repeat}");
+            } else
+            {
+                Console.WriteLine(ThreadName.Value);
+            }
+            Console.WriteLine(ThreadName.Value);
+        }
         static void Main(string[] args)
         {
-            Task t1 = new Task(delegate ()
-            {
-                _lock.WriteLock();
-                count++;
-                _lock.WriteUnlock();
-            });
+            ThreadPool.SetMinThreads(1, 1);
+            ThreadPool.SetMaxThreads(3, 3);
+            Parallel.Invoke(WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI);
 
-            Task t2 = new Task(delegate ()
-            {
-                _lock.WriteLock();
-                count--;
-                _lock.WriteUnlock();
-            });
-
-            t1.Start();
-            t2.Start();
-
-            Task.WaitAll(t1, t2);
-
-            Console.WriteLine(count);
+            ThreadName.Dispose();
         }
     }
 }
