3be360e252025526a4b86b33be38c3284fec2c59
King0wl <70518135+yhj0076@users.noreply.github.com>
2024-12-04 20:26:20 +0900

    소켓프로그래밍 입문

--- DummyClient/Program.cs
+++ DummyClient/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace DummyClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // DNS(Domain Name System
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+
+            // 휴대폰 설정
+            Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                // 문지기한테 입장 문의
+                socket.Connect(endPoint);
+                Console.WriteLine($"Connected To {socket.RemoteEndPoint}");
+
+                // 보낸다
+                byte[] sendBuff = Encoding.UTF8.GetBytes("Hello World!");
+                int sendByte = socket.Send(sendBuff);
+
+                // 받는다
+                byte[] recvBuff = new byte[1024];
+                int recvByte = socket.Receive(recvBuff);
+                string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvByte);
+                Console.WriteLine($"[From Server] : {recvData}");
+
+                //나간다
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -1,31 +1,62 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 using System.Threading;
 
 namespace ServerCore
 {
     class Program
     {
-        static ThreadLocal<string> ThreadName = new ThreadLocal<string>(() => { return $"My Name Is {Thread.CurrentThread.ManagedThreadId}"; });
-        static void WhoAmI()
-        {
-            bool repeat = ThreadName.IsValueCreated;
-            if (repeat) 
-            {
-                Console.WriteLine(ThreadName.Value + "{repeat}");
-            } else
-            {
-                Console.WriteLine(ThreadName.Value);
-            }
-            Console.WriteLine(ThreadName.Value);
-        }
         static void Main(string[] args)
         {
-            ThreadPool.SetMinThreads(1, 1);
-            ThreadPool.SetMaxThreads(3, 3);
-            Parallel.Invoke(WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI);
+            // DNS(Domain Name System
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
-            ThreadName.Dispose();
+            // www.gomi.com -> 123.123.123.123
+
+            // 문지기
+            Socket listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                // 문지기 교육
+                listenSocket.Bind(endPoint);
+
+                // 영업 시작
+                // backlog : 최대 대기수
+                listenSocket.Listen(10);
+
+                while (true)
+                {
+                    Console.WriteLine("Listening...");
+
+                    // 손님을 입장시킨다.
+                    Socket clientSocket = listenSocket.Accept();
+
+                    // 받는다.
+                    byte[] buffer = new byte[1024];
+                    int recvBytes = clientSocket.Receive(buffer);
+                    string recvData = Encoding.UTF8.GetString(buffer, 0, recvBytes);
+                    Console.WriteLine($"[From Client] {recvData}");
+
+                    // 보낸다.
+                    byte[] buffer2 = Encoding.UTF8.GetBytes("Welcome to MMORPG Server !");
+                    clientSocket.Send(buffer2);
+
+                    // 쫓아낸다.
+                    clientSocket.Shutdown(SocketShutdown.Both);
+                    clientSocket.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
         }
     }
 }
