abcf06823e8f3937f37dfa3765fc7dcd21f00b07
King0wl <70518135+yhj0076@users.noreply.github.com>
2025-04-04 19:37:19 +0900

    Unity Link #1
    유니티 연동 #1

--- Common/Packet/GenPackets.bat
+++ Common/Packet/GenPackets.bat
@@ -1,5 +1,8 @@
 START ../../PacketGenerator/bin/PacketGenerator.exe ../../PacketGenerator/PDL.xml
 XCOPY /Y GenPackets.cs "../../DummyClient/Packet"
+XCOPY /Y GenPackets.cs "C://Users/yhj00/Connection/Assets/Script/_ServerControl/Client/Packet"
 XCOPY /Y GenPackets.cs "../../Server/Packet"
+
 XCOPY /Y ClientPacketManager.cs "../../DummyClient/Packet"
+XCOPY /Y ClientPacketManager.cs "C://Users/yhj00/Connection/Assets/Script/_ServerControl/Client/Packet"
 XCOPY /Y ServerPacketManager.cs "../../Server/Packet"
